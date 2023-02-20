b5edcdef61363d826d61b6c2134ef796894849d0
Köhler, Benjamin (EXTERN: ITUC) <benjamin.koehler@ituc.de>
2023-02-20 14:26:31 +0100

    highlight on yank

--- init.lua
+++ init.lua
@@ -16,3 +16,15 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('HighlightYank', {}),
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
+
