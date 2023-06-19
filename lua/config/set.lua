daddb9c87019ef8a1f95c891872dac6d295fd215
Benjamin Köhler <benjaminkoehler89@gmail.com>
2023-06-19 14:16:48 +0200

    fix icons and remap autocomplete

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -33,3 +33,4 @@ vim.opt.showmode = false
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -9,9 +9,12 @@ return {
             vim.g['undotree_WindowLayout'] = '2'
         end
     },
-    { "nvim-lualine/lualine.nvim", config = true },
+    { "nvim-lualine/lualine.nvim", config = true,
+      dependencies = { "nvim-tree/nvim-web-devicons" }
+    },
     { "numToStr/Comment.nvim", config = true },
     { "lukas-reineke/indent-blankline.nvim", config = true }, -- Add indentation guides even on blank lines
     { "tpope/vim-sleuth" },                    -- Detect tabstop and shiftwidth automatically
     { "/windwp/nvim-autopairs", config = true },
+    { "mfussenegger/nvim-dap" }
 }
