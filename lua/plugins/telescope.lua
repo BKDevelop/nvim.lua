7c630d9a91dcd661c2df76d8e7cee7dc64cdede2
Benjamin Köhler <benjaminkoehler89@gmail.com>
2024-02-13 16:56:11 +0100

    fuzzy find highlighted text

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,17 @@
+-- fuzzy find highlighted text
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
+
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ''
+	end
+end
+
 return {
     {
         'nvim-telescope/telescope.nvim',
@@ -7,7 +21,18 @@ return {
               defaults = {
                 wrap_results = true,
               },
+
             }
+
+			local keymap = vim.keymap.set
+			local tb = require('telescope.builtin')
+			local opts = { noremap = true, silent = true }
+
+			keymap('v', '<space>G', function()
+				local text = vim.getVisualSelection()
+				tb.live_grep({ default_text = text })
+			end, opts)
+
         end,
         dependencies = { 'nvim-lua/plenary.nvim' }
     }
