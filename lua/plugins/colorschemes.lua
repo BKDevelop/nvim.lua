013de678ed69bba014f39984a531dc918ed1878b
Benjamin Köhler <benjaminkoehler89@gmail.com>
2024-08-24 11:58:59 +0200

    gruvbox is bae

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,12 +1,14 @@
 return {
     -- the colorscheme should be available when starting Neovim
     {
-        "catppuccin/nvim",
+        "sainnhe/gruvbox-material",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             -- load the colorscheme here
-            vim.cmd([[colorscheme catppuccin-macchiato]])
+            -- vim.cmd([[colorscheme catppuccin-macchiato]])
+            vim.cmd([[colorscheme gruvbox-material]])
         end,
     },
+    { "catppuccin/nvim"}
 }
